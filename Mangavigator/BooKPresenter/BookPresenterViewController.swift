009865242cc7565a9df4654ee9b6044742038d73
axl411 <axl411511@live.com>
2018-08-01 00:17:35 +0900

    add ZIPFoundation

--- Mangavigator/BooKPresenter/BookPresenterViewController.swift
+++ Mangavigator/BooKPresenter/BookPresenterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import ZIPFoundation
 
 class BookPresenterViewController: NSViewController {
     private let file: URL
@@ -27,5 +28,42 @@ class BookPresenterViewController: NSViewController {
         super.viewDidLoad()
         view.wantsLayer = true
         view.layer?.backgroundColor = NSColor.cyan.cgColor
+
+        guard let archive = Archive(url: file, accessMode: .read) else  {
+            return
+        }
+
+        let firstFile = archive.first { entry in
+            entry.type == .file
+//            entry.path
+        }
+
+        do {
+            try archive.extract(firstFile!) { data in
+                let image = NSImage(data: data)!
+                let imageView = NSImageView(image: image)
+                imageView.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview(imageView)
+                NSLayoutConstraint.activate([
+                    imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                    imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                    imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+                    imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8)
+                    ])
+            }
+            let path = try FileManager.default.url(
+                for: .cachesDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+            ).appendingPathComponent(UUID().uuidString)
+            try archive.extract(firstFile!, to: path)
+            let image = NSImage(contentsOf: path)!
+            // TODO: design a mechanism for displaying images in zip file (thumbnails & real image & preloading)
+
+        }
+        catch {
+
+        }
     }
 }
