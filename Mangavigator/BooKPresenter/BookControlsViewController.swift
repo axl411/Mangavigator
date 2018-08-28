25834f8c0bf76999d149b05b772e9a784e2a85b6
axl411 <axl411511@live.com>
2018-08-29 02:20:39 +0900

    add todo

--- Mangavigator/BooKPresenter/BookControlsViewController.swift
+++ Mangavigator/BooKPresenter/BookControlsViewController.swift
@@ -49,6 +49,7 @@ class BookControlsViewController: NSViewController {
             textField.topAnchor.constraint(equalTo: view.topAnchor, constant: nameLabelInset),
             textField.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+        // TODO: auto hide book title
     }
 
     override func viewDidLayout() {
